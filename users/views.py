1fa3a6f6510599b09b7b8d5c2622453723ccf55c
thaonguyen0229 <47158178+thaonguyen0229@users.noreply.github.com>
2020-05-02 23:50:22 -0400

    .

--- users/views.py
+++ users/views.py
@@ -10,4 +10,5 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
     form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+    #return render(request)
 
