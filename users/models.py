1dba3b8bbb7ac0c99a5440e0d8b7151a752397d3
thaonguyen0229 <47158178+thaonguyen0229@users.noreply.github.com>
2020-05-13 21:03:08 -0400

    Add Profile model
    -extend User model
    -teacher or student option
    -test Profile using admin site

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = [
+        ('STU', 'Student'),
+        ('TEA', 'Teacher')
+    ]
+    userRole = models.CharField(
+        max_length=3,
+        choices=ROLE_CHOICES,
+        default='STU'
+    )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
